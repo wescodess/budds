11ddbda3082f12ffb2c1ab18147ebb54417b1ae2
kaypappi <wesleyukadike@yahoo.com>
2026-04-21 23:43:41 -0400

    fix(layout): pass-through routes fill full width with flex wrapper

--- app/pages/app/folders/[id]/quiz.vue
+++ app/pages/app/folders/[id]/quiz.vue
@@ -1,3 +1,5 @@
 <template>
-  <NuxtPage :page-key="$route.fullPath" />
+  <div class="flex min-h-0 min-w-0 flex-1 flex-col overflow-hidden">
+    <NuxtPage :page-key="$route.fullPath" />
+  </div>
 </template>
